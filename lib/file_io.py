2206cd4e609804ece45c6699e7f25d4942de1b12
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-29 11:17:19 +0300

    write_file function to overwrite content in text working

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,8 @@
 def write_file(file_name, file_content):
-    pass
+    extension= ".txt"
+    string =f"/{file_name}{extension}"
+    with open(string, mode="w", encoding="utf-8") as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
     pass
